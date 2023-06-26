b5d7aee5513f0d79c33aa7ac1eb80b28e3226f26
Ofir <ofirfirst@gmail.com>
2023-06-26 14:48:29 +0300

    lets see

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-hey i mhgete
+def rewrite_file(file_path, new_content):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(new_content)
+        print(f"File '{file_path}' has been rewritten successfully.")
+    except IOError:
+        print(f"An error occurred while rewriting the file '{file_path}'.")
